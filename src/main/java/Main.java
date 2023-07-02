b4b1166fe78f35ab5c9521993bee41186c383003
Ruslan-Shev <134603263+Ruslan-Shev@users.noreply.github.com>
2023-07-02 18:06:44 +0300

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        System.out.println("Bonus: " + bonus);
+    }
 }
