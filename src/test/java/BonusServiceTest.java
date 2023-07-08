9bc701d96808fb8188b7186f66a88226ad0297af
Ruslan-Shev <134603263+Ruslan-Shev@users.noreply.github.com>
2023-07-08 13:40:20 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,16 +1,32 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class BonusServiceTest {
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+      BonusService service = new BonusService();
+
+      // подготавливаем данные:
+      long amount = 1_000_000;
+      boolean registered = true;
+      long expected = 500;
+
+      // вызываем целевой метод:
+      long actual = service.calculate(amount, registered);
+
+      // производим проверку (сравниваем ожидаемый и фактический):
+      Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
+        long amount = 100_000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 50;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -20,13 +36,29 @@ public class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateForRegisteredAndOverLimit() {
+    void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000;
-        boolean registered = true;
-        long expected = 500;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100_000;
+        boolean registered = false;
+        long expected = 0;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
